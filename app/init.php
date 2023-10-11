07fd8ff73d6b4f75e472b47bf56b125b1e3128f2
Fernando Sanchez <ferhsncz@gmail.com>
2023-10-11 00:31:56 -0600

    reestructuracion finalizada

---
+++ app/init.php
@@ -0,0 +1,6 @@
+<?php
+    spl_autoload_register(function($lib){
+        require_once 'lib/' . $lib . '.php';
+        require_once 'config/config.php';
+    });
+?>
---
+++ app/views/inc/footer.php
@@ -0,0 +1,5 @@
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+  <!-- ejemplo: <script src="<? // = URL . '/js/bootstrap.bundle.min.js' ?>"></script> -->
+
+</body>
+</html>
