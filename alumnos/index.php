4b176be8ec3ff5bc0fa1d3410e8484b5e40bc880
Fernando Sanchez <ferhsncz@gmail.com>
2023-11-06 09:43:59 -0600

    Index de alumno cambiado. Imagenes agregadas a la carpeta img.

--- alumnos/index.php
+++ alumnos/index.php
@@ -3,120 +3,222 @@
     include '../config.php'; 
     /*Este codigo manda al archivo index.html si se trata de ingresar al dashboard sin haber iniciado sesion*/
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Psicopedagogia</title>
+    <link rel="shortcut icon" href="favicon.ico">
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+  </head>
+  <body>
+    <!-- Barra de navegacion -->
+    <nav class="navbar navbar-dark bg-dark">
+      <div class="container-fluid">
+        <!-- Logo -->
+        <a class="navbar-brand" href="#">
+          <img src="../img/favicon.ico" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">Psicopedagogia
+        </a>
+        <!-- Boton del dashboard -->
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <!-- inicio Dashboard -->
+        <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" data-bs-scroll="false" data-bs-backdrop="static" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+          <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Menu</h5>
+            <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          </div>
+          <div class="offcanvas-body">
+            <!-- cuerpo del Dashboard -->
+            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+              <li class="mx-auto d-block">
+                <img src="../img/user.png" alt="" height="150px" width="150px">
+              </li>
+              <li class="mx-auto d-block" style="font-size: 1.8rem; text-align: center;">
+                <!-- Nombre del usuario -->
+                <?php echo ($_SESSION['nombre']); //Muestro el nombre de quien inicio sesion?>
+              </li>
+              <li class="mx-auto d-block">
+                <div class="btn-group mx-auto" role="group" aria-label="Button group with nested dropdown">
+                  <!-- <button type="button" class="btn btn-outline-primary">Cambiar contraseña</button> -->
+                  <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#cerrar">Cerrar Sesion</button>
+                </div>
+              </li>
+              <li><br></li>
+              <li>
+                <div class="accordion" id="accordionExample">
+                  <!-- Acordion de Tests -->
+                  <div class="accordion-item bg-dark">
+                    <h2 class="accordion-header">
+                      <button class="accordion-button bg-dark text-white" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        <i class="fa-solid fa-book-open" style="margin-right: 0.5rem;"></i>Examenes
+                      </button>
+                    </h2>
+                    <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                      <div class="accordion-body text-white">
+                        <div class="list-group list-group-flush">
+                          <a href="test-lyn.php" class="list-group-item list-group-item-action bg-dark text-white"><i class="fa-solid fa-pen" style="margin-right: 0.5rem;"></i>Canal de aprendizaje de preferencia</a>
+                          <a href="honey-form.php" class="list-group-item list-group-item-action bg-dark text-white"><i class="fa-solid fa-pen" style="margin-right: 0.5rem;"></i>Estilos de aprendizaje (Honey Alonso)</a>
+                          <a href="PNL.php" class="list-group-item list-group-item-action bg-dark text-white"><i class="fa-solid fa-pen" style="margin-right: 0.5rem;"></i>Estilos de aprendizaje (Modelo PNL)</a>
+                          <a href="riesgospsicosociales.php" class="list-group-item list-group-item-action bg-dark text-white"><i class="fa-solid fa-pen" style="margin-right: 0.5rem;"></i>Cuestionario de Riesgos Psicosociales</a>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li><br></li>
 
-<!DOCTYPE html>
-<html lang="es">
-    <head>
-        <meta charset="UTF-8"/>
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-        <title>Psicopedagogia | Alumno</title>
-        <!-- Estilos CSS locales -->
-        <link href="../css/styles.css" rel="stylesheet" />
-        <!-- Fontawesome -->
-        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
-    </head>
-    <body class="sb-nav-fixed">
-        <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-            <!-- Navbar Brand-->
-            <a class="navbar-brand ps-3" href="dash_alumno.php">Psicopedagogia</a>
-            <!-- Sidebar Toggle-->
-            <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-            <!-- Navbar-->
-            <ul class="navbar-nav ml-auto ms-md-0 me-3 me-lg-4">
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Conectado como: 
-                        <?php echo ($_SESSION['nombre']); //Muestro el nombre de quien inicio sesion?><i class="fas fa-user fa-fw"></i>
-                    </a>
-                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <li><a class="dropdown-item" href="logout.php">Cerrar sesión</a></li>
-                    </ul>
-                </li>
+              <!-- Botoles de los modales -->
+              <li>
+                <div class="btn-group">
+                  <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal1">
+                    Aviso de privacidad
+                  </button>
+                  <button type="button" class="btn btn-outline-primary" data-bs-toggle="modal" data-bs-target="#exampleModal2">
+                    Politica de privacidad
+                  </button>
+                </div>
+              </li>
+              
             </ul>
-        </nav>
-        <div id="layoutSidenav">
-            <div id="layoutSidenav_nav">
-                <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-                    <div class="sb-sidenav-menu">
-                        <div class="nav">
-                            <div class="sb-sidenav-menu-heading">Dashboard</div>
-                            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-                                <div class="sb-nav-link-icon"><i class="fas fa-columns"></i></div>
-                                Tests
-                                <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-                            </a>
-                            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-                                <nav class="sb-sidenav-menu-nested nav">
-                                    <a class="nav-link" href="test-lyn.php">Linn O´Brien</a>
-                                    <a class="nav-link" href="honey-form.php">Honey Alonso</a>
-                                    <a class="nav-link" href="pnl.php">Modelo PNL</a>
-                                </nav>
-                            </div>
-                        </div>
-                    </div>
-                </nav>
-            </div>
-            <div id="layoutSidenav_content">
-                <main>
-                    <div class="container-fluid px-4">
-                        <h1 class="mt-4">Bienvenido</h1>
-                        <ol class="breadcrumb mb-4">
-                            <li class="breadcrumb-item active">Vista Estudiante</li>
-                        </ol>
-                        <div class="row">
-                            <div class="col">
-                                <div class="card bg-primary text-white mb-4">
-                                    <div class="card-body text-center">Test Canal de Aprendizaje de Preferencia Lynn O’Brien (1990)</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="test-lyn.php">Realizar</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="card bg-warning text-white mb-4">
-                                    <div class="card-body text-center text-white">Cuestionario Honey-Alonso de Estilos de Aprendizaje</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="honey-form.php">Realizar</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body text-center">Test de Estilo de Aprendizaje (Modelo PNL)</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="PNL.php">Realizar</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col">
-                                <div class="card bg-success text-white mb-4">
-                                    <div class="card-body text-center">Test 4</div>
-                                    <div class="card-footer d-flex align-items-center justify-content-between">
-                                        <a class="small text-white stretched-link" href="riesgospsicosociales.php">Realizar</a>
-                                        <div class="small text-white"><i class="fas fa-angle-right"></i></div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>                      
-                    </div>
-                </main>
-                <footer class="py-4 bg-light mt-auto">
-                    <div class="container-fluid px-4">
-                        <div class="d-flex align-items-center justify-content-between small">
-                            <div class="text-muted">Copyright &copy; Psicopedagogia web</div>
-                            <div>
-                                <a href="#">Politicas de privacidad</a>
-                                &middot;
-                                <a href="#">Terminos &amp; Condiciones</a>
-                            </div>
-                        </div>
-                    </div>
-                </footer>
-            </div>
+          </div>
         </div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-    </body>
+      </div>
+    </nav>
+    <!-- fin del navbar -->
+    
+    <!-- Modales -->
+
+      <!-- Modal 1 -->
+      <div class="modal fade" id="exampleModal1" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Aviso de privacidad</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              En Psicopedagogía, valoramos y respetamos su privacidad. Todos los datos personales recopilados a través de nuestros tests se mantienen estrictamente 
+              confidenciales y se utilizan únicamente con fines de evaluación y mejora del aprendizaje. No compartimos, vendemos ni divulgamos su información a 
+              terceros sin su consentimiento.
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Modal 2 -->
+      <div class="modal fade" id="exampleModal2" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Politica de privacidad</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              Nuestra política de privacidad está diseñada para proteger la información personal de nuestros usuarios. Puede encontrar información detallada sobre 
+              cómo recopilamos, almacenamos y utilizamos sus datos en nuestra Política de Privacidad. Respetamos todas las leyes de protección de datos y estamos 
+              comprometidos con mantener su información segura.
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Ok</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <!-- Modal Cerrar sesion -->
+      <div class="modal fade" id="cerrar" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">Cerrar sesion</h1>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              ¿Estas seguro de que quieres cerrar sesion?
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Cancelar</button>
+              <button type="button" class="btn btn-danger" data-bs-dismiss="modal">Cerrar Sesion</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+    <!-- Inicio del contenido -->
+    <div style="background-color: rgb(184, 184, 184);">
+      <div class="container">
+
+        <!-- MVC -->
+
+        <div class="row" style="padding: 1rem;">
+          <div class="col" style="padding: 1rem;">
+            <div class="card" style="width: 18rem;">
+              <img src="../img/test.png" class="card-img-top" alt="Imagen-test">
+              <div class="card-body">
+                <h5 class="card-title">Canal de aprendizaje de preferencia</h5>
+                <p class="card-text">Este test es el primer test xd</p>
+                <a href="test-lyn.php" class="btn btn-primary">Iniciar test</a>
+              </div>
+            </div>
+          </div>
+          <div class="col" style="padding: 1rem;">
+            <div class="card" style="width: 18rem;">
+              <img src="../img/test.png" class="card-img-top" alt="Imagen-test">
+              <div class="card-body">
+                <h5 class="card-title">Estilos de aprendizaje (Honey Alonso)</h5>
+                <p class="card-text">Este test es el segundo test xd</p>
+                <a href="honey-form.php" class="btn btn-primary">Iniciar test</a>
+              </div>
+            </div>
+          </div>
+          <div class="col" style="padding: 1rem;">
+            <div class="card" style="width: 18rem;">
+              <img src="../img/test.png" class="card-img-top" alt="Imagen-test">
+              <div class="card-body">
+                <h5 class="card-title">Estilos de aprendizaje (Modelo PNL)</h5>
+                <p class="card-text">Este test es el tercer test xd</p>
+                <a href="PNL.php" class="btn btn-primary">Iniciar test</a>
+              </div>
+            </div>
+          </div>
+          <div class="col" style="padding: 1rem;">
+            <div class="card" style="width: 18rem;">
+              <img src="../img/test.png" class="card-img-top" alt="Imagen-test">
+              <div class="card-body">
+                <h5 class="card-title">Cuestionario de Riesgos Psicosociales</h5>
+                <p class="card-text">Este test es el cuarto test xd</p>
+                <a href="riesgospsicosociales.php" class="btn btn-primary">Iniciar test</a>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <!-- MVC -->
+
+      </div> 
+    </div>
+    <!-- Fin del contenido -->
+    
+    <!-- Footer -->
+    <footer class="bg-dark text-center text-lg-start">
+      <div class="text-center text-white p-3">
+        ©Psicopedagogia 2023
+      </div>
+    </footer>
+    <!-- Fin del Footer -->
+
+    <!-- JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  </body>
 </html>
